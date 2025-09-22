a6b7ee7f6de1ece7625c26cf459a9dbd134bd38b
weijianming <weijianming@costrip.cn>
2025-09-22 21:06:15 +0800

    docs(wxsocket): 更新 token 描述信息，补充公司测试用途

--- wxsocket/token.js
+++ wxsocket/token.js
@@ -7,7 +7,7 @@
 module.exports = {
   // 数组里可以是对象（有 token 字段）
   token: [
-    { token: 'BVLjqT4j81Z6sDBUaxrf', callTheSquare: '', describe: '测试demo' },
+    { token: 'BVLjqT4j81Z6sDBUaxrf', callTheSquare: '', describe: '测试demo，公司测试使用' },
     { token: 'abc123' },
   ],
 };
